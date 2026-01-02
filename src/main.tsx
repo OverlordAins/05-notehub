fa6929281d8497f06706258e1ed40e23bb30664d
Daniil <stifflingdagger@gmail.com>
2026-01-02 03:12:32 +0200

    fast fix style import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './components/App/App.tsx';
-import './index.css';
+import './main.css';
 
 const queryClient = new QueryClient();
 
